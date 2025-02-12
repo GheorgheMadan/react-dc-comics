a2f1f95d742987bb07ce3fde9b54245ace420bb6
Gheorghe <madangheorghe.30@gmail.com>
2025-02-12 17:33:37 +0100

    create main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,35 @@
 export default function Main() {
     return (
         <main>
-            <h2>ciao</h2>
+            <section id="container-content">
+                <div>
+                    <p> --&gt; Content goes here &lt;-- </p>
+                </div>
+            </section>
+            <section id="container-cards">
+                <div id="col">
+                    <div className="card">
+                        <img src="./public/img/buy-comics-digital-comics.png" alt="" className="imgCard" />
+                        <span>DIGITAL COMICS</span>
+                    </div>
+                    <div className="card">
+                        <img src="./public/img/buy-comics-merchandise.png" alt="" className="imgCard" />
+                        <span>DC MERCHANDISE</span>
+                    </div>
+                    <div className="card">
+                        <img src="./public/img/buy-comics-subscriptions.png" alt="" className="imgCard" />
+                        <span>SUBSCRIPTION</span>
+                    </div>
+                    <div className="card">
+                        <img src="./public/img/buy-comics-shop-locator.png" alt="" className="imgLocator" />
+                        <span>COMIC SHOP LOCATOR</span>
+                    </div>
+                    <div className="card">
+                        <img src="./public/img/buy-dc-power-visa.svg" alt="" className="imgCard" />
+                        <span>DC POWER VISA</span>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 } 
